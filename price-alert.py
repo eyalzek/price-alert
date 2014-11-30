4bb35b0be30298c70665bd76bdb8a6149bd1ea89
vagelim <vagelim@gmail.com>
2014-11-29 20:25:05 -0500

    Update price-alert.py
    changes to the configuration file handling
    changed the item handler to be able to work with multiple items

--- price-alert.py
+++ price-alert.py
@@ -5,16 +5,27 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from lxml import html
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read("creds")
+email_user = config.get('email','user')
+email_pass = config.get('email','pass')
+
+
 
 # Change these values to your desired sale page (the selector and price check were tested on Amazon).
-URL = "http://www.amazon.com/exec/obidos/ASIN/B0042A8CW2/websearchengi40-20"
+BASE_URL = "http://www.amazon.com/exec/obidos/ASIN/"
 SMTP_URL = "smtp.gmail.com:587"
 XPATH_SELECTOR = '//*[@id="priceblock_ourprice"]'
 MAX_PRICE = "$100"
 SLEEP_INTERVAL = 10
 
+#ITEMS is a list of lists, storing ASINs and their maximum prices
+
+ITEMS = [['B0042A8CW2',100]]
 def send_email(price):
-    global URL
+    global BASE_URL
     email, passwd = [line.strip("\n") for line in tuple(open("creds", "r"))] # replace this with your email credentials
 
     try:
@@ -36,17 +47,22 @@ def send_email(price):
         print("Message has been sent.")
 
 while True:
-    r = requests.get(URL)
-    tree = html.fromstring(r.text)
-    try:
-        price = tree.xpath(XPATH_SELECTOR)[0].text
-    except IndexError:
-        print("Didn't find the 'price' element, trying again")
-        continue
-    if price <= MAX_PRICE:
-        print("Price is {}!! Trying to send email.".format(price))
-        send_email(price)
-        break
-    else:
-        print("Price is {}. Ignoring...".format(price))
-        time.sleep(SLEEP_INTERVAL)
+#each[0] is the item's ASIN while each[1] is that item's maximum price
+	for each in ITEMS:
+		
+		r = requests.get(BASE_URL + each[0])
+		tree = html.fromstring(r.text)
+		try:
+			#We have to strip the dollar sign off of the number to cast it to a float
+			price = float(tree.xpath(XPATH_SELECTOR)[0].text[1:])
+		except IndexError:
+			print("Didn't find the 'price' element, trying again")
+			continue
+		if price <= each[1]:
+			print("Price is {}!! Trying to send email.".format(price))
+			send_email(price)
+			break
+		else:
+			print("Price is {}. Ignoring...".format(price))
+	print "Sleeping for " + str(SLEEP_INTERVAL)
+	time.sleep(SLEEP_INTERVAL)
