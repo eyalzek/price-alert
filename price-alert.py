7b095b54ccfcc777be6b06a1883adb5d56c26c19
eyalzek <eyal.ez@gmail.com>
2016-07-10 11:42:29 +0200

    some changes to address issue #2

--- price-alert.py
+++ price-alert.py
@@ -2,6 +2,7 @@
 import time
 import requests
 import smtplib
+from urlparse import urljoin
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from lxml import html
@@ -14,12 +15,12 @@ email_pass = config.get('email','pass')
 
 
 # Change these values to your desired sale page (the selector and price check were tested on Amazon).
-BASE_URL = "http://www.amazon.com/exec/obidos/ASIN/"
+BASE_URL = "https://www.amazon.co.uk/gp/offer-listing/"
 SMTP_URL = "smtp.gmail.com:587"
-XPATH_SELECTOR = '//*[@id="priceblock_ourprice"]'
+XPATH_SELECTOR = '//*[contains(@class, "olpOfferPrice")]'
 SLEEP_INTERVAL = 10
 #ITEMS is a list of lists, storing ASINs and their maximum prices
-ITEMS = [['B0042A8CW2',100]]
+ITEMS = [("B014GXQ9YW", 80)]
 
 def send_email(price):
     global BASE_URL
@@ -44,21 +45,22 @@ def send_email(price):
 
 while True:
     #item[0] is the item's ASIN while item[1] is that item's maximum price
-	for item in ITEMS:
-		r = requests.get(BASE_URL + item[0])
-		tree = html.fromstring(r.text)
-		try:
-			#We have to strip the dollar sign off of the number to cast it to a float
-			price = float(tree.xpath(XPATH_SELECTOR)[0].text[1:])
-		except IndexError:
-			print("Didn't find the 'price' element, trying again")
-			continue
-		if price <= item[1]:
-			print("Price is {}!! Trying to send email.".format(price))
-			send_email(price)
-			break
-		else:
-			print("Price is {}. Ignoring...".format(price))
+    for item in ITEMS:
+        r = requests.get(urljoin(BASE_URL, item[0]), verify=False)
+        tree = html.fromstring(r.text)
+        try:
+            elements = tree.xpath(XPATH_SELECTOR)
+            #We have to strip the currency sign off of the number to cast it to a float
+            prices = [float(p.text.lstrip().rstrip()[1:]) for p in elements]
+        except IndexError:
+            print("Didn't find the 'price' element, trying again")
+            continue
+        if any(map(lambda p: p <= item[1], prices)):
+            print("Prices are {}!! Trying to send email.".format(prices))
+            send_email(price)
+            break
+        else:
+            print("Prices are {}. Ignoring...".format(prices))
 
-	print "Sleeping for {} seconds".format(SLEEP_INTERVAL)
-	time.sleep(SLEEP_INTERVAL)
+    print "Sleeping for {} seconds".format(SLEEP_INTERVAL)
+    time.sleep(SLEEP_INTERVAL)
