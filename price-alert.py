a67448e249750e6e5b704e9202655cf8ebd798cc
Eyal Zekaria <eyal.ez@gmail.com>
2018-01-28 01:11:12 +0100

    Use python3, use logging module, formatting

--- price-alert.py
+++ price-alert.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os
 import re
@@ -7,9 +7,10 @@ import time
 import requests
 import smtplib
 import argparse
-import urlparse
+import logging
 from copy import copy
 from lxml import html
+from urllib.parse import urljoin
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -20,9 +21,9 @@ def send_email(price, url, email_info):
         s.starttls()
         s.login(email_info['user'], email_info['password'])
     except smtplib.SMTPAuthenticationError:
-        print('Failed to login')
+        logger.info('Failed to login')
     else:
-        print('Logged in! Composing message..')
+        logger.info('Logged in! Composing message..')
         msg = MIMEMultipart('alternative')
         msg['Subject'] = 'Price Alert - %s' % price
         msg['From'] = email_info['user']
@@ -32,7 +33,7 @@ def send_email(price, url, email_info):
         part = MIMEText(text, 'plain')
         msg.attach(part)
         s.sendmail(email_info['user'], email_info['user'], msg.as_string())
-        print('Message has been sent.')
+        logger.info('Message has been sent.')
 
 
 def get_price(url, selector):
@@ -46,15 +47,24 @@ def get_price(url, selector):
     try:
         # extract the price from the string
         price_string = re.findall('\d+.\d+', tree.xpath(selector)[0].text)[0]
-        print(price_string)
+        logger.info(price_string)
         return float(price_string.replace(',', '.'))
-    except IndexError, TypeError:
-        print('Didn\'t find the \'price\' element, trying again later...')
+    except (IndexError, TypeError) as e:
+        logger.debug(e)
+        logger.info('Didn\'t find the \'price\' element, trying again later')
 
 
 def get_config(config):
     with open(config, 'r') as f:
         return json.loads(f.read())
+
+
+def config_logger(debug):
+    global logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
 
 
 def parse_args():
@@ -65,35 +75,39 @@ def parse_args():
                         help='Configuration file path')
     parser.add_argument('-t', '--poll-interval', type=int, default=30,
                         help='Time in seconds between checks')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Enable debug level logging')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    config_logger(args.debug)
     config = get_config(args.config)
     items = config['items']
 
     while True and len(items):
         for item in copy(items):
-            print('Checking price for %s (should be lower than %s)' % (
+            logger.info('Checking price for %s (should be lower than %s)' % (
                 item[0], item[1]))
-            item_page = urlparse.urljoin(config['base_url'], item[0])
+            item_page = urljoin(config['base_url'], item[0])
             price = get_price(item_page, config['xpath_selector'])
             if not price:
                 continue
             elif price <= item[1]:
-                print('Price is %s!! Trying to send email.' % price)
+                logger.info('Price is %s!! Trying to send email.' % price)
                 send_email(price, item_page, config['email'])
                 items.remove(item)
             else:
-                print('Price is %s. Ignoring...' % price)
+                logger.info('Price is %s. Ignoring...' % price)
 
         if len(items):
-            print('Sleeping for %d seconds' % args.poll_interval)
+            logger.info('Sleeping for %d seconds' % args.poll_interval)
             time.sleep(args.poll_interval)
         else:
             break
-    print('Price alert triggered for all items, exiting.')
+    logger.info('Price alert triggered for all items, exiting.')
+
 
 if __name__ == '__main__':
     main()
